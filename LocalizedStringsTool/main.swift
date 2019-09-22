a5100184e9de671f7c14c80245b9364c50078b5c
AndrewPetrov <andrew.petrov.dev@gmail.com>
2019-09-22 21:17:58 +0300

    Duplication and unused keys works

--- LocalizedStringsTool/main.swift
+++ LocalizedStringsTool/main.swift
@@ -14,10 +14,12 @@ import Foundation
  + получить список всех .swift, .h, .m файлов
  + прочитать из каждого строки
  + составить сет из этих ключей “usedKeys”
- - получить список файлов .strings
- - составить массив из ключей “availableKeys” для каждого языка
+ + получить список файлов .strings
+ + составить массив из ключей “availableKeys” для каждого языка
  - “usedKeys” -   “availableKeys” = ключи без перевода
- - “availableKeys” - “usedKeys”  =  неиспользуемые ключи
+ + “availableKeys” - “usedKeys”  =  неиспользуемые ключи
+ + дублирование переводов
+
  */
 
 let argCount = CommandLine.argc
@@ -58,46 +60,60 @@ while let element = enumerator?.nextObject() as? String {
 
 var swiftStrings = Set<String>()
 var mStrings = Set<String>()
-var localizedStrings = [String: [String]]()
+//                     [lang: [key: translation]
+var localizedStrings = [String: [String: String]]()
 
 let swiftPattern = #""(\S*)".localized\(\)"#
 // lang(@"shake_find_devices")
 let objCPattern = #"lang\(@"(\S*)"\)"#
-let localizedStringPattern = #""(\S*)" = ".*";"#
+let localizedStringPattern = #""(\S*)" = "(.*)";"#
 
-func matchingStrings(regex: String, text: String) -> [String] {
+// [0]  = whole regex result, we need only groups
+func matchingStrings(regex: String, text: String, neededRegexGroups: [Int] = [1]) -> [[String]] {
     guard let regex = try? NSRegularExpression(pattern: regex, options: []) else { return [] }
 
     let nsText = text as NSString
     let results = regex.matches(in: text, options: [], range: NSMakeRange(0, text.count))
+    dump(results)
 
     return results.map { result in
-        let aaa = result.range(at: 1).location != NSNotFound
-            ? nsText.substring(with: result.range(at: 1))
-            : ""
-        print(aaa)
-        return aaa
+        neededRegexGroups.map {
+            result.range(at: $0).location != NSNotFound
+                ? nsText.substring(with: result.range(at: $0))
+                : nil
+        }
+        .compactMap { $0 }
     }
 }
 
 swiftSet.forEach { swiftFilePath in
     if let fileText = try? String(contentsOf: URL(fileURLWithPath: path + "/" + swiftFilePath), encoding: .utf8) {
-        matchingStrings(regex: swiftPattern, text: fileText).forEach { swiftStrings.insert($0) }
+        matchingStrings(regex: swiftPattern, text: fileText).map { $0.first }.compactMap { $0 }.forEach { swiftStrings.insert($0) }
     }
 }
 
 mSet.forEach { mFilePath in
     if let fileText = try? String(contentsOf: URL(fileURLWithPath: path + "/" + mFilePath), encoding: .utf8) {
-        matchingStrings(regex: objCPattern, text: fileText).forEach { mStrings.insert($0) }
+        matchingStrings(regex: objCPattern, text: fileText).map { $0.first }.compactMap { $0 }.forEach { mStrings.insert($0) }
     }
 }
 
 localizableDict.forEach { dirPath in
     let paath = path + "/" + dirPath + "/" + "Localizable.strings"
     print(paath)
-    
+
     if let fileText = try? String(contentsOf: URL(fileURLWithPath: paath), encoding: .utf16) {
-          localizedStrings[dirPath] = matchingStrings(regex: localizedStringPattern, text: fileText)
+        let element: [String: String] = matchingStrings(regex: localizedStringPattern, text: fileText, neededRegexGroups: [1, 2])
+            .map { (smallArray: [String]) -> [String: String] in
+                [smallArray.first ?? "": smallArray.last ?? ""]
+            }
+            .reduce([String: String]()) { (result: [String: String], value: [String: String]) in
+                var newDict = result
+                newDict[value.keys.first ?? ""] = value.values.first ?? ""
+
+                return newDict
+            }
+        localizedStrings[dirPath] = element
     }
 }
 
@@ -105,3 +121,32 @@ print(swiftStrings)
 print(mStrings)
 
 print(localizedStrings)
+
+let combinedUsedLocalizedStrings = swiftStrings.union(mStrings)
+
+//          [lang: Set<key>]
+let keysSet = localizedStrings.mapValues { Set($0.keys) }
+let translationsSet = localizedStrings.mapValues { Set($0.values) }
+let extraTranslations = keysSet.mapValues { $0.subtracting(combinedUsedLocalizedStrings) }
+
+// let crossReference = Dictionary(grouping: contacts, by: { $0.phone })
+let duplicatedTranslations = localizedStrings.mapValues { (oneLangDict: [String: String]) -> [String] in
+
+    var valuesUsing = [String]()
+    var extraKeys = [String]()
+
+    oneLangDict.forEach { (arg: (key: String, value: String)) in
+        let (key, value) = arg
+        if valuesUsing.contains(value) {
+            extraKeys.append(key)
+        } else {
+            valuesUsing.append(value)
+        }
+    }
+
+    return extraKeys
+}
+
+print("\n\n")
+print("extraTranslations", extraTranslations)
+print("duplicatedTranslations", duplicatedTranslations)
