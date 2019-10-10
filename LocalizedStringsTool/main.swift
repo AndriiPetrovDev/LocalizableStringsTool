80f78c26aa2efd826019ed79b1b4947fc9a41ef1
AndrewPetrov <andrew.petrov.dev@gmail.com>
2019-10-10 23:02:32 +0300

    works on mono

--- LocalizedStringsTool/main.swift
+++ LocalizedStringsTool/main.swift
@@ -16,14 +16,21 @@ import Foundation
  + составить сет из этих ключей “usedKeys”
  + получить список файлов .strings
  + составить массив из ключей “availableKeys” для каждого языка
- - “usedKeys” -   “availableKeys” = ключи без перевода
+ + “usedKeys” -   “availableKeys” = ключи без перевода
  + “availableKeys” - “usedKeys”  =  неиспользуемые ключи
  + дублирование переводов
+ - добавить все исключения из моно
+ - поддержка обж с
+ - указание пути
+ - рефакторинг
+ - кастомные правила для ключей
+ - кастомные исключения
 
  */
 
 let argCount = CommandLine.argc
-let path = "/Users/Shared/Relocated Items/Security/develop/MONOBANK/app-ios-client/Koto"
+//let path = "/Users/Shared/Relocated Items/Security/develop/MONOBANK/app-ios-client/Koto"
+let path = "/Users/Shared/Relocated Items/Security/develop/MONOBANK/app-ios-client/Mono"
 
 let manager = FileManager.default
 let enumerator = manager.enumerator(atPath: path)
@@ -37,7 +44,6 @@ let content = try? manager.contentsOfDirectory(atPath: FileManager.default.curre
 // print(content)
 
 while let element = enumerator?.nextObject() as? String {
-//    print(element)
     if element.hasSuffix(".swift") {
         swiftFilePathSet.insert(element)
     } else if element.hasSuffix(".h") {
@@ -61,16 +67,16 @@ var allSwiftProbablyKeys = Set<String>()
 var localizedStrings = [String: [String: String]]()
 
 let swiftPattern = #""(?<KEY>\S*)".localized\(\)"#
-// lang(@"shake_find_devices")
+
 let objCPattern = #"lang\(@"(?<KEY>\S*)"\)"#
 let localizedStringPattern = #""(?<KEY>\S*)" = "(.*)\s?";"#
-//let allSwiftStringPattern = #""(?<KEY>\S*)""#
+
 let allObjCtStringPattern = #"@"(?<KEY>\S*)""#
 
-let allSwiftStringPattern = #"(?<!(UIImage\(named: ))(?<!(Animatiion: ))(?<!(animation: ))(?<!(withAnimation: ))(?<!(Animation.named\())(?<!(#imageLiteral\(resourceName: ))(?:"(?<KEY>([a-z0-9]+[_])+[a-z0-9]+)")*(?:"(?<ANYSTRING>\S*)")*"#
+let allSwiftStringPattern = #"(?<!(forResource: ))(?<!(SegmentedBarItemImageSet\())(?<!(appendingPathComponent\())(?<!(forKey: ))(?<!(userInfo\[))(?<!(UIImage\(named: ))(?<!(Animatiion: ))(?<!(animation: ))(?<!(withAnimation: ))(?<!(Animation.named\())(?<!(#imageLiteral\(resourceName: ))(?:"(?<KEY>((?<!ic_)[a-z0-9]+[_])+[a-z0-9]+)")*(?:"(?<ANYSTRING>\S*)")*"#
 
 // [0]  = whole regex result, we need only groups
-//    func matchingStrings(regex: String, text: String, neededRegexGroups: [Int] = [1]) -> [[String]] {
+
 func matchingStrings(regex: String, text: String, names: [String] = ["KEY"]) -> [[String]] {
     guard let regex = try? NSRegularExpression(pattern: regex, options: []) else { return [] }
 
@@ -87,15 +93,6 @@ func matchingStrings(regex: String, text: String, names: [String] = ["KEY"]) -> 
                 }
                 .compactMap { $0 }
         }
-
-//    return results.map { result in
-//        neededRegexGroups.map {
-//            result.range(at: $0).location != NSNotFound
-//                ? nsText.substring(with: result.range(at: $0))
-//                : nil
-//        }
-//        .compactMap { $0 }
-//    }
 }
 
 swiftFilePathSet.forEach { swiftFilePath in
@@ -120,8 +117,12 @@ localizableFilePathDict.forEach { dirPath in
     print(paath)
     do {
         // Koto utf8, Mono utf16
-        let fileText8 = try String(contentsOf: URL(fileURLWithPath: paath), encoding: .utf8)
-        let element8: [String: String] = matchingStrings(regex: localizedStringPattern, text: fileText8, names: ["KEY", "ANYSTRING"])
+          let fileText8 = try? String(contentsOf: URL(fileURLWithPath: paath), encoding: .utf8)
+          let fileText16 = try? String(contentsOf: URL(fileURLWithPath: paath), encoding: .utf16)
+        let arr = [fileText8, fileText16].compactMap { $0 }
+        
+        if let fileText = arr.first {
+        let element: [String: String] = matchingStrings(regex: localizedStringPattern, text: fileText, names: ["KEY", "ANYSTRING"])
             .map { (smallArray: [String]) -> [String: String] in
                 [smallArray.first ?? "": smallArray.last ?? ""]
             }
@@ -131,32 +132,15 @@ localizableFilePathDict.forEach { dirPath in
 
                 return newDict
             }
-
-        let fileText16 = try String(contentsOf: URL(fileURLWithPath: paath), encoding: .utf16)
-        let element16: [String: String] = matchingStrings(regex: localizedStringPattern, text: fileText16, names: ["KEY", "ANYSTRING"])
-            .map { (smallArray: [String]) -> [String: String] in
-                [smallArray.first ?? "": smallArray.last ?? ""]
-            }
-            .reduce([String: String]()) { (result: [String: String], value: [String: String]) in
-                var newDict = result
-                newDict[value.keys.first ?? ""] = value.values.first ?? ""
-
-                return newDict
-            }
-
-        localizedStrings[dirPath] = element16.merging(element8) { (first, _) -> String in
-            first
+            
+            localizedStrings[dirPath] = element
         }
-
-    } catch {
-        print(error)
-    }
 }
 
 print(swiftStrings)
 print(mStrings)
 
-print("localizedStrings = ", localizedStrings)
+//print("localizedStrings = ", localizedStrings)
 
 let combinedUsedLocalizedStrings = swiftStrings.union(mStrings)
 
